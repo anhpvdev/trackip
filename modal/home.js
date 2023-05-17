bb8b7dd3ddddc7d639e3757dec5afdf04a303bd5
anhpvdev <anhpv.dev@gmai.com>
2023-05-17 17:29:21 +0700

    asd

--- modal/home.js
+++ modal/home.js
@@ -2,11 +2,11 @@ const home= {
     trackip:(req, res) => {        
         var ip1 = req.headers['x-real-ip']
         var ip2 = req.connection.remoteAddress
-        var ip2 = req.headers['x-forwarded-for'] 
-        var ip2 = req.socket.remoteAddress 
+        var ip3 = req.headers['x-forwarded-for'] 
+        var ip4 = req.socket.remoteAddress 
 
 
-        var check = ip1+ " || " + ip2+ " || " + ip3+ " || " + ip4
+        var check = ip1+ " ||read: " + ip2+ " || " + ip3+ " || " + ip4
         res.send(check);
     }
 
