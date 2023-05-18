2f85212092975e8d518fd23fe3742271e62d195a
anhpvdev <anhpv.dev@gmai.com>
2023-05-18 18:33:04 +0700

    asdsd

--- modal/home.js
+++ modal/home.js
@@ -19,7 +19,6 @@ const home= {
        
         if(!cokie){
             res.cookie(`Some`,`Thing...`,{
-                maxAge: 2*60*1000,
                 httpOnly: true
             });
             var ip = req.headers['x-forwarded-for']
